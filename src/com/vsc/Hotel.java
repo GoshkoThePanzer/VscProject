35ae56d9f9906f2589d29f2e2df03be5cd3a24a4
Georgi Ignatov <ginchothetree@gmail.com>
2020-02-16 19:08:50 +0200

    first commit

---
+++ src/com/vsc/Hotel.java
@@ -0,0 +1,9 @@
+package com.vsc;
+
+public class Hotel {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+    }
+}

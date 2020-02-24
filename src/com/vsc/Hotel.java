b58421fe78b3f7ef146152dec9066dc5ce486cfa
Georgi Ignatov <ginchothetree@gmail.com>
2020-02-24 18:08:06 +0200

    I am trying to understand how this thing works I think my answer belongs here

--- src/com/vsc/Hotel.java
+++ src/com/vsc/Hotel.java
@@ -5,5 +5,6 @@ public class Hotel {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+        System.out.println("Am I commiting?");
     }
 }

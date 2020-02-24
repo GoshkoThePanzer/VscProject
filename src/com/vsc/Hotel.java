5d9300d86f5d0afdae8218458abbdc6cc7928ea6
Georgi Ignatov <ginchothetree@gmail.com>
2020-02-24 18:18:25 +0200

    It should probably work now?

--- src/com/vsc/Hotel.java
+++ src/com/vsc/Hotel.java
@@ -6,5 +6,9 @@ public class Hotel {
 	// write your code here
         System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAa");
         System.out.println("Am I commiting?");
+        System.out.println("Scary little green men");
+        System.out.println("Do you believe in scary little green men?");
+        System.out.println("It's the end!");
+        System.out.println("It's not working!");
     }
 }

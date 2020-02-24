0ebf86124a5d55a73b87bbadcf870f06ae0d84cd
Georgi Ignatov <ginchothetree@gmail.com>
2020-02-24 18:21:32 +0200

    Okay, it is working indeed, ths is the last test

--- src/com/vsc/Hotel.java
+++ src/com/vsc/Hotel.java
@@ -10,5 +10,7 @@ public class Hotel {
         System.out.println("Do you believe in scary little green men?");
         System.out.println("It's the end!");
         System.out.println("It's not working!");
+        System.out.println("Now it's working! :)");
+        System.out.println("Okay I am very dumb so I am just going to use commit and git push origin master, rly sorry, I don't understand this git.");
     }
 }
